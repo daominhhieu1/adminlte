3bebf92bf02942c631e42aacc9eaf5e72f202dad
Đào Minh Hiếu <38307049+daominhhieu1@users.noreply.github.com>
2022-07-27 11:03:34 +0700

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,20 @@
+
+DATE=`date +%Y.%m.%d.%H.%M`
+DOCKER_IMAGE=manage_admin
+DOCKER_CONTAINER=manage_admin
+
+## Build docker
+docker build  --no-cache -t $DOCKER_IMAGE .;
+
+# build master
+## Stop and delete service master
+if [ $(docker ps -qa -f name=\/$DOCKER_CONTAINER\$) ]; then\
+	docker stop $DOCKER_CONTAINER;\
+    docker rm $DOCKER_CONTAINER;\    
+fi
+
 docker run -dit  --restart always \
 -p 5000:5000 \
---name manage_admin manage_admin npm start
+--name $DOCKER_CONTAINER  $DOCKER_IMAGE npm start
+
+docker image prune -af 
