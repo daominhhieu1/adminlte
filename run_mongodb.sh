880dfb728307850968f77b41a3e042f1b8da897d
Đào Minh Hiếu <38307049+daominhhieu1@users.noreply.github.com>
2022-07-27 11:28:03 +0700

    Update run_mongodb.sh

--- run_mongodb.sh
+++ run_mongodb.sh
@@ -1,9 +1,15 @@
+DATE=`date +%Y.%m.%d.%H.%M`
+DOCKER_IMAGE=mongodb
+DOCKER_CONTAINER=mongodb
 NETWORK_NAME=net-node
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create ${NETWORK_NAME}; 
 fi
-
-sudo docker run --name mongodb \
+if [ $(docker ps -qa -f name=\/$DOCKER_CONTAINER\$) ]; then\
+	docker stop $DOCKER_CONTAINER;\
+    docker rm $DOCKER_CONTAINER;\    
+fi
+sudo docker run --name $DOCKER_IMAGE \
 --net $NETWORK_NAME \
 -v /mongodata_docker:/data/db \
 -p 27017:27017 \
