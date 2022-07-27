734f985dc6ff8de202d1896143c59d7df563f9d3
Đào Minh Hiếu <38307049+daominhhieu1@users.noreply.github.com>
2022-07-27 11:26:42 +0700

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
-
+echo $(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongodb)
 DATE=`date +%Y.%m.%d.%H.%M`
 DOCKER_IMAGE=manage_admin
 DOCKER_CONTAINER=manage_admin
@@ -6,6 +6,10 @@ DOCKER_CONTAINER=manage_admin
 ## Build docker
 docker build  --no-cache -t $DOCKER_IMAGE .;
 
+NETWORK_NAME=net-node
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
+     docker network create ${NETWORK_NAME} ; 
+fi
 # build master
 ## Stop and delete service master
 if [ $(docker ps -qa -f name=\/$DOCKER_CONTAINER\$) ]; then\
@@ -14,6 +18,7 @@ if [ $(docker ps -qa -f name=\/$DOCKER_CONTAINER\$) ]; then\
 fi
 
 docker run -dit  --restart always \
+--net $NETWORK_NAME \
 -p 5000:5000 \
 --name $DOCKER_CONTAINER  $DOCKER_IMAGE npm start
 
